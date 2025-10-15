fc43f8456461423e8934f431f2c3ccec4c5a31e9
wguii <guizap26262626@hotmail.com>
2025-10-15 16:14:17 -0300

    initial commit

---
+++ Source/UI/LfoComponent.cpp
@@ -0,0 +1,26 @@
+#include <JuceHeader.h>
+#include "LfoComponent.h"
+
+//==============================================================================
+LfoComponent::LfoComponent (juce::AudioProcessorValueTreeState& apvts, juce::String lfoFreqId, juce::String lfoDepthId)
+: lfoFreq ("LFO Freq", lfoFreqId, apvts, dialWidth, dialHeight)
+, lfoDepth ("LFO Depth", lfoDepthId, apvts, dialWidth, dialHeight)
+{
+    addAndMakeVisible (lfoFreq);
+    addAndMakeVisible (lfoDepth);
+}
+
+LfoComponent::~LfoComponent()
+{
+}
+
+void LfoComponent::resized()
+{
+    const auto width = 70;
+    const auto height = 88;
+    const auto startY = 40;
+        
+    lfoFreq.setBounds (18, startY, width, height);
+    lfoDepth.setBounds (90, startY, width, height);
+}
+
